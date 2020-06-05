27b4888478fb19cf39fde8dcde5e8e102d734917
hanna <annisokay97@gmail.com>
2020-06-05 10:57:58 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { ThemeProvider } from 'ustudio-ui/theme';
+
+const App = () => {
+  return (
+    <ThemeProvider>
+      <h1>Hello world</h1>
+    </ThemeProvider>
+  );
+};
+
+export default App;
