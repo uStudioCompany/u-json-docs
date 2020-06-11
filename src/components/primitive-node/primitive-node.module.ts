863996f477694c3f4f94a0b4a80dadd918b0b4bc
semenov-ol <semenov.os16@gmail.ocm>
2020-06-11 15:33:25 +0300

    refactor: Change types in parseSchemaType func

--- src/components/primitive-node/primitive-node.module.ts
+++ src/components/primitive-node/primitive-node.module.ts
@@ -1,6 +1,8 @@
-export const parseSchemaType = (schema: any, parent: any) => {
+import { JSONSchema7, JSONSchema7TypeName } from 'json-schema';
+
+export const parseSchemaType = (schema: JSONSchema7, parent: string) => {
   if (Array.isArray(schema.type)) {
-    return schema.type.map((type: any) => JSON.stringify(type, null, 2)).join(' | ');
+    return schema.type.map((type: JSONSchema7TypeName) => JSON.stringify(type, null, 2)).join(' | ');
   }
 
   if (parent === 'array' && schema.items && !Array.isArray(schema.items) && typeof schema.items !== 'boolean') {
